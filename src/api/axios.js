9d80086cfd9b31ef883bc6f8ac40a6c1b07b3bd4
Ivan Cirovic <ivan.cirovic994@gmail.com>
2026-01-26 23:19:43 +0100

    Prepare frontend for deploy

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://task-manager-backend-gat8.onrender.com/api",
 });
 api.interceptors.request.use((config)=>{
     const user = JSON.parse(localStorage.getItem("user"));
