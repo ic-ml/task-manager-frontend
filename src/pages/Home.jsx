805ea104b6eed5ee5299df95aef434a8ea254297
Ivan Cirovic <ivan.cirovic994@gmail.com>
2026-02-01 18:59:32 +0100

    fix/filteredTask let instead const

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ const [sort, setSort] = useState("");
     );
   }
 
-  const filteredTasks = tasks.filter(
+  let filteredTasks = tasks.filter(
     (task) => task.status === activeTab
   );
   if (search) {
